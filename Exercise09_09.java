b6da835b85c48d776a5dc50ec00909c6f82f6b5b
margaret3991 <margaret.lanphere@gmail.com>
2021-04-13 00:17:24 -0700

    bad java

--- Exercise09_09.java
+++ Exercise09_09.java
@@ -3,9 +3,10 @@
 // Chapter 9: Programming Project 2
 
 public class Exercise09_09 {
-   public static void main(String[] args) {
+   public static void main(String[] args) 
+            {
       //Test 
-      RegularPolygon P1 = new RegularPolygon();
+            RegularPolygon P1 = new RegularPolygon();
       RegularPolygon P2 = new RegularPolygon(6,4);
       RegularPolygon P3 = new RegularPolygon(10,4,5.6,7.8);
       
